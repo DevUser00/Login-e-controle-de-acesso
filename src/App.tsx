468998cd899efa004621c047c7f52057c0f2b571
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-15 19:29:05 -0300

    Layout ADMIN

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,32 @@ import ClientHome from './routes/ClientHome';
 import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import { ContextCartCount } from './utils/context-cart';
+import Admin from './routes/Admin';
+import AdminHome from './routes/Admin/AdminHome';
 
 function App() {
 
   const [contextCartCount, setContextCartCount] = useState<number>(0);
 
   return (
-    <ContextCartCount.Provider value={{contextCartCount, setContextCartCount}}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
+    <ContextCartCount.Provider value={{ contextCartCount, setContextCartCount }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
             <Route path="product-details/:productId" element={<ProductDetails />} />
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
+          </Route>
+          <Route path='/admin/' element={<Admin />} >
+            <Route index element={<AdminHome />} />
 
-        </Route>
-        <Route path="*" element={<Navigate to={"/"}/>} />
+          </Route>
+          <Route path="*" element={<Navigate to={"/"} />} />
 
-      </Routes>
-    </BrowserRouter >
+        </Routes>
+      </BrowserRouter >
     </ContextCartCount.Provider>
   );
 }
--- src/components/HeaderCliet/index.tsx
+++ src/components/HeaderCliet/index.tsx
@@ -1,30 +1,16 @@
 import './style.css';
 
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import CartIcon from '../CartIcon';
+import HeaderAdmin from '../HeaderAdmin';
 
 
 export default function HeaderClient() {
 
   return (
-    <header className="dsc-header-client">
-      <nav className="dsc-container">
-        <Link to={`/`}>
-          <h1>DSCommerce</h1>
-        </Link>
-        <div className="dsc-navbar-right">
-          <div className="dsc-menu-items-container">
-            <Link to={`/cart`}>
-              <div className="dsc-menu-item">
-                <CartIcon />
-              </div>
-            </Link>
-          </div>
-          <Link to={`/login`}>
-            Entrar
-          </Link>
-        </div>
-      </nav>
-    </header>
+    <>
+    <HeaderAdmin></HeaderAdmin>
+    <Outlet></Outlet>
+    </>
   );
 }
