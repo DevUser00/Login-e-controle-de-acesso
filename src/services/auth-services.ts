40a1d5154e84ecc828a435bc7e427b9828d563f0
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-19 11:26:18 -0300

    hasAnyRole

--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import QueryString from "qs";
-import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { requestBackend } from "../utils/requests";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import * as accessTokenRepository from '../localstorege/access-token-repository'
@@ -64,4 +64,21 @@ export function isAuthenticated(): boolean {
    let tokenPayload = getAccessTokenPayload();
    //verificar ser exp do user é maior que Date.now
    return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+//Teste: usuário possui algum dos perfis informados
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+   if (roles.length === 0) {
+      return true;
+   }
+   const tokenPayload = getAccessTokenPayload();
+   if (tokenPayload !== undefined) {
+      for (var i = 0; i < roles.length; i++) {
+         if (tokenPayload.authorities.includes(roles[i])) {
+            return true;
+         }
+      }
+      //return roles.some(role => tokenData.authorities.includes(role));
+   }
+   return false;
 }
