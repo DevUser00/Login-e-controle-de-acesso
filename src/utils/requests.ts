ce7a7264f3248f808da2e033adc170519a888f01
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-19 09:12:35 -0300

    Requestbackend update withCreadentils

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,8 +1,18 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
-
+import * as authService from './../services/auth-services'
 
 // facilitates request for backend with future token...
 export function requestBackend(config: AxiosRequestConfig) {
-   return axios( {...config, baseURL: BASE_URL} ); 
+
+   //testando se for v ele acresenta o token
+  const headers = config.withCredentials ? 
+  {... config.headers,    
+   Authorization : "Bearer " + authService.getAccessToken()
+  }
+  : config.headers;
+
+
+
+   return axios( {...config, baseURL: BASE_URL, headers} ); 
 }
