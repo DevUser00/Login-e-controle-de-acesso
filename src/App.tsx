0844472bc90c8c8fab481bb15e724e1b8dca4cd2
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-12-21 20:19:32 -0300

    Page confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { AccessTokenPayloadDTO } from './models/auth';
 import { ContextToken } from './utils/context-token';
 import * as authService from './services/auth-services'
 import * as cartService from './services/cart-services'
+import Confirmation from './routes/ClientHome/Confirmation';
 
 function App() {
 
@@ -43,10 +44,11 @@ function App() {
           <Routes>
             <Route path="/" element={<ClientHome />}>
               <Route index element={<Catalog />} />
-              <Route path="catalog" element={<Catalog />} />
+              <Route path="catalog" element={<Catalog />} />  
               <Route path="product-details/:productId" element={<ProductDetails />} />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
+              <Route path="confirmation/:orderId" element={<Confirmation />} />
             </Route>
             <Route path='/admin/' element={
               <PrivateRoute roles={['ROLE_ADMIN']} >
